006a6f16c5205d3c51960e117df017f97630c919
peekaboo-4869 <1515689121@qq.com>
2013-04-03 22:31:17 +0800

    update checkNet

--- src/cn/iebook/wifi/CheckNet.java
+++ src/cn/iebook/wifi/CheckNet.java
@@ -5,6 +5,7 @@ public class CheckNet {
 	public void checkNet() {
 		System.out.println("aa");
 		System.out.println("bb");
+		System.out.println("bbbb");
 //		/* 判断客户端优选网络 */
 //		if (getFirstNet()) {
 //			logger.d("----....----.....---->>>first net is wifi");
